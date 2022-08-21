84ef79f7cec96bf1400d3ae210bb1b4210f3a6bd
musanurbadr <musanur6685@gmail.com>
2022-08-21 19:05:06 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,30 @@
+def Articles():
+    articles =[
+        {
+            'id':1,
+            'title': 'Arlicle one',
+            'body': 'loyghyhu  ghbngy bh uygh ',
+            'alsdkf':'aldflkasdjflk',
+            'asddfda':'15-07-2007'
+
+        },
+        {
+            'id':2,
+            'title': 'Arlicle two',
+            'body': 'loyghyhu  ghbngy bh uygh ',
+            'aother':'badr',
+            'asddfda':'15-07-2007'
+
+        },
+        {
+            'id':3,
+            'title': 'Arlicle three',
+            'body': 'loyghyhu  ghbngy bh uygh ',
+            'aother':'musa',
+            'asddfda':'15-07-2007'
+
+        }
+
+
+    ]
+    return articles
